51dde166d1e0359755b2122c9b8c60f9c10f7d57
Antar Espadas <antar.espadas@hotmail.com>
2023-12-05 01:46:17 -0600

    Add day 3 part 1

--- AdventOfCode2023/Program.fs
+++ AdventOfCode2023/Program.fs
@@ -1,27 +1,23 @@
 ﻿
-open Day2.Problem1
-open Day2.Problem2
+open Day3.Problem1
 
 open System.IO
 
-let lines = File.ReadAllLines "/home/antar/repos/AdventOfCode2023/Day2/Input.txt"
+let lines = File.ReadAllLines "/home/antar/repos/AdventOfCode2023/Day3/Input.txt"
 
-// let lines = [
-//     "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-//     "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
-//     "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
-//     "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
-//     "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
-// ]
-//
-let bag = Map.ofSeq [
-    "red", 12
-    "green", 13
-    "blue", 14
-]
+// let lines = [|
+//     "467..114.."
+//     "...*......"
+//     "..35..633."
+//     "......#..."
+//     "617*......"
+//     ".....+.58."
+//     "..592....."
+//     "......755."
+//     "...$.*...."
+//     ".664.598.."
+// |]
 
-let records = parseLines lines
-
-records
+lines
 |> solve
-|> (fun x -> printfn $"{x}")
+|>(fun x -> printfn $"%A{x}")
---
+++ Day3/Problem1.fs
@@ -0,0 +1,81 @@
+module Day3.Problem1
+
+open System
+open System.Collections.Generic
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Core
+
+type PuzzleNumber = {
+    value: int
+    first: int
+    last: int
+    row: int
+}
+
+let getAdjacentIndices number =
+    let left = number.first - 1
+    let right = number.last + 1
+    let top =
+        seq { left..right }
+        |> Seq.map (fun x -> x, number.row + 1)
+    let bottom =
+        seq { left..right }
+        |> Seq.map (fun x -> x, number.row - 1)
+    Seq.concat [top; bottom; [left, number.row; right, number.row]]
+    
+let tryGetChar (cells: string array) (x, y) =
+    try Some (cells[y][x])
+    with | _ -> None
+    
+let getAdjacentChars cells number =
+    getAdjacentIndices number
+    |> Seq.choose (tryGetChar cells)
+
+let isDigit c =
+    '0' <= c && c <= '9'
+    
+let isDot c =
+    c = '.'
+    
+let isSymbol c = not (isDot c || isDigit c)
+
+
+let hasAdjacentSymbol cells number =
+    getAdjacentChars cells number
+    |> Seq.filter isSymbol
+    |> (not << Seq.isEmpty)
+    
+let toGrid (rows: string array) =
+    rows
+    |> Array.map (fun x -> x.ToCharArray())
+    
+type State =
+    | Skipping
+    | Reading
+
+
+let getPuzzleNumber row (m: Match) =
+    let value = Int32.Parse m.Value
+    {
+        value = value
+        first = m.Index
+        last = m.Index + m.Length - 1
+        row = row
+    }
+    
+let re = Regex "\\d+"
+let getNumbers row (str: string) =
+    re.Matches str
+    |> Seq.map (getPuzzleNumber row)
+    
+let getAllNumbers (lines: string array) =
+    lines
+    |> Seq.mapi getNumbers
+    |> Seq.collect id
+    
+let solve (lines: string array) =
+    lines
+    |> getAllNumbers
+    |> Seq.map (fun x -> x.value, hasAdjacentSymbol lines x)
+    |> Seq.choose (fun (x, y) -> if y then Some(x) else None)
+    |> Seq.sum
